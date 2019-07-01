bc5b28f03e085567bff311755c7b27b0d61710b9
sergey <paksergey94@gmail.com>
2019-07-02 09:15:34 +1000

    Initial commit

---
+++ src/EmailValidator.php
@@ -0,0 +1,37 @@
+<?php
+
+declare(strict_types=1);
+
+namespace SPakV;
+
+/**
+ * Email validator.
+ */
+class EmailValidator {
+
+	/**
+	 * Validate email.
+	 *
+	 * @param string $email Email
+	 *
+	 * @return bool
+	 */
+	public static function validate(string $email): bool {
+		$regExp = '/^([a-z0-9\+_\-]+)(\.[a-z0-9\+_\-]+)*@([a-z0-9\-]+\.)+[a-z]{2,6}$/ix';
+
+		return (bool) preg_match($regExp, $email);
+	}
+
+	/**
+	 * Validate mx record of email.
+	 *
+	 * @param string $email Email
+	 *
+	 * @return bool
+	 */
+	public static function validateMx(string $email): bool {
+		$domain = substr(strrchr($email, "@"), 1);
+
+		return checkdnsrr($domain, 'MX');
+	}
+}
